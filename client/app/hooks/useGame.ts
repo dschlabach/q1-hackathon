f1e6ecabf93c1724b20a8f568c738fc2d626ca02
dschlabach <daniel.schlabach3@gmail.com>
2025-01-30 14:55:42 -0800

    chore: add test JSON

--- client/app/hooks/useGame.ts
+++ client/app/hooks/useGame.ts
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
 import { publicSupabase } from "@/database/client";
 
+const TEST_GAME_JSON = JSON.stringify([
+	{
+		agent_id: 1,
+		text: "this is some text for a round",
+		health: 100,
+	},
+	{
+		agent_id: 2,
+		text: "this is some text for a round",
+		health: 90,
+	},
+	{
+		agent_id: "ORCHESTRATOR",
+		text: "ORCHESTRATOR text",
+	},
+]);
+
 /**
  * Streams game events from the Supabase realtime API
  * @param gameId - The ID of the game to fetch
  * @returns The game data
  */
 export const useGame = (gameId: string) => {
-	const [game, setGame] = useState(null);
+	const [game, setGame] = useState(TEST_GAME_JSON);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
