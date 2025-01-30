763b0d7f0de85ca0cfe7de83a9a2441988465434
dschlabach <daniel.schlabach3@gmail.com>
2025-01-30 10:52:29 -0800

    init

---
+++ client/app/api/games/[gameId]/route.ts
@@ -0,0 +1,20 @@
+export async function GET(
+	request: Request,
+	{ params }: { params: { gameId: string } },
+) {
+	// TODO: implement GET
+}
+
+export async function POST(
+	request: Request,
+	{ params }: { params: { gameId: string } },
+) {
+	// TODO: implement POST
+}
+
+export async function PATCH(
+	request: Request,
+	{ params }: { params: { gameId: string } },
+) {
+	// TODO: implement PATCH
+}
