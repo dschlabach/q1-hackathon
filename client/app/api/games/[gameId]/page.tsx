e6f8048dfd12e6cd5378c81805be0a7ebce72351
dschlabach <daniel.schlabach3@gmail.com>
2025-01-30 14:39:58 -0800

    feat: add hook to game id page

--- client/app/api/games/[gameId]/page.tsx
+++ client/app/api/games/[gameId]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useGame } from "@/app/hooks/useGame";
 import { useParams } from "next/navigation";
 
 export default function GamePage() {
 	const { gameId } = useParams();
+	const game = useGame(gameId as string);
 
 	return <div>Game {gameId}</div>;
 }
