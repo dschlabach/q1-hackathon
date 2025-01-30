231afb1e9c908cbaaa8c64cf392ab61c35844a58
dschlabach <daniel.schlabach3@gmail.com>
2025-01-30 15:59:08 -0800

    fix: fix route

--- client/app/api/games/[gameId]/route.ts
+++ client/app/api/games/[gameId]/route.ts
@@ -60,7 +60,7 @@ export async function PATCH(
 		.eq("game_id", Number(gameId));
 
 	// Initialize game via serverless function
-	await fetch(`${API_URL}/api/battle`, {
+	await fetch(`${API_URL}/api/games/battle`, {
 		method: "POST",
 		body: JSON.stringify({
 			gameId,
